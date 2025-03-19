05e6762c1f43b4ac3c822407ca2b2c375ffa1bbd
Matthieu Viry <matthieu.viry@cnrs.fr>
2025-03-19 12:03:03 +0100

    Update build stack

--- rollup.config.js
+++ rollup.config.js
@@ -2,7 +2,7 @@
 import commonjs from "@rollup/plugin-commonjs";
 import noderesolve from "@rollup/plugin-node-resolve";
 import babel from "@rollup/plugin-babel";
-import { terser } from "rollup-plugin-terser";
+import terser from '@rollup/plugin-terser';
 
 export default {
   input: "src/index.js",
